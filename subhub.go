c82125efb422789697de20ce0a8225e5d3fc0bfc
Sean Duffy <seanduffy@live.com>
2014-08-25 23:47:30 +0100

    Functions for getting user subscriptions and channel uploads playlists

--- subhub.go
+++ subhub.go
@@ -1,11 +1,42 @@
 package main
 
 import (
-	_ "fmt"
+	"database/sql"
 	"log"
+	"os"
 
 	"code.google.com/p/google-api-go-client/youtube/v3"
+	_ "github.com/mattn/go-sqlite3"
 )
+
+// UserSubscriptionIds returns a list of ChannelIds belonging
+// to the user's subscribed channels
+func UserSubscriptionIds(service *youtube.Service) ([]string, error) {
+	call := service.Subscriptions.List("snippet").Mine(true).MaxResults(2)
+
+	response, err := call.Do()
+	if err != nil {
+		return nil, err
+	}
+
+	channelIds := []string{}
+	for _, subscription := range response.Items {
+		channelIds = append(channelIds, subscription.Id)
+	}
+
+	return channelIds, nil
+}
+
+func ChannelUploadsPlaylistId(service *youtube.Service, channelId string) (string, error) {
+	call := service.Channels.List("contentDetails").Id(channelId)
+
+	response, err := call.Do()
+	if err != nil {
+		return "", err
+	}
+
+	return response.Items[0].ContentDetails.RelatedPlaylists.Uploads, nil
+}
 
 func main() {
 	client, err := buildOAuthHTTPClient(youtube.YoutubeReadonlyScope)
@@ -18,6 +49,25 @@ func main() {
 		log.Fatalf("Error creating YouTube client: %v", err)
 	}
 
-	call := service.Subscriptions.List("snippet").Mine(true)
+	db, err := sql.Open("sqlite3", os.ExpandEnv("$HOME/db.sqlite"))
+	if err != nil {
+		log.Fatalf("Could not open database: %v", err)
+	}
 
+	err = db.QueryRow(`
+    CREATE TABLE IF NOT EXISTS
+    videos (
+        id integer primary key,
+        publishedAt datetime,
+        channelId integer)
+    `).Scan()
+	if err != nil && err != sql.ErrNoRows {
+		log.Fatalf("Could not create table: %v", err)
+	}
+
+	userSubscriptionIds, err := UserSubscriptionIds(service)
+
+	for _, channelId := range userSubscriptionIds {
+		log.Printf("%v\n", channelId)
+	}
 }
